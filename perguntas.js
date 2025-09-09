de9615b14eb13a0ab55aec1bb084751cf366b2e7
yuri_naiz <naiz.yuri@escola.pr.gov.br>
2025-09-09 08:47:09 -0300

    09/09

---
+++ perguntas.js
@@ -0,0 +1,59 @@
+criaCartao(
+'categoria'
+'Quanto tempo a luz do Sol demora para chegar à Terra?'
+'8 minutos.'
+)
+
+criaCartao(
+'categoria'
+'Em que ano o homem pisou na lua?'
+'1969.'
+)
+
+criaCartao(
+'categoria'
+'Qual ave é capaz de bater as asas até 200 vezes por segundo?'
+'o beija-flor.'
+)
+
+criaCartao(
+'categoria'
+'Quantos ossos temos no nosso corpo?'
+'206.'
+)
+
+criaCartao(
+'categoria'
+'Qual é o menor país do mundo?'
+'Vaticano.'
+)
+
+criaCartao(
+'categoria'
+'Quantas teclas tem um piano?'
+'88'
+)
+
+criaCartao(
+'categoria'
+'Por qual apelido ficou conhecido o jogador de futebol Edson Arantes?'
+'Pelé.'
+)
+
+criaCartao(
+'categoria'
+'O que é filantropo?'
+' Pessoa que pratica a caridade.'
+)
+
+criaCartao(
+'categoria'
+'pergunta'
+'resposta'
+)
+
+criaCartao(
+'categoria'
+'pergunta'
+'resposta'
+)
