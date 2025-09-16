fe36c0857e47b926280b56766cdf1b0e171cc2b2
yuri_naiz <naiz.yuri@escola.pr.gov.br>
2025-09-16 10:48:15 -0300

    16/09

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-function criaCartao(categoria, pergunta, resposta){
+function criaCartao(categoria, pergunta, resposta) {
     let container = document.getElementById("container");
     let cartao = document.createElement("article");
     cartao.className = 'cartao'
@@ -12,11 +12,16 @@ function criaCartao(categoria, pergunta, resposta){
     <div class="cartao__conteudo__resposta">
     <p>${resposta}</p>
     </div>
-    </div> `
+    </div>`
     
+let respostaEstaVisivel = false
+
+function viraCartao(){
+    respostaEstaVisivel=!respostaEstavisivel
+}
+
+
+
     container. appendChild(cartao)
-
-
-
 
 }
