ccedef6540ea9e3b4a01f4fa23a708ea6655ebf5
yuri_naiz <naiz.yuri@escola.pr.gov.br>
2025-09-23 10:45:37 -0300

    23/09

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ function criaCartao(categoria, pergunta, resposta) {
     let cartao = document.createElement('article');
     cartao.className = 'cartao'
 
-    cartao.innerHTML=`
+    cartao.innerHTML = `
     <div class="cartao__conteudo">
     <h3>${categoria}</h3>
     <div class="cartao__conteudo__pergunta">
@@ -13,17 +13,17 @@ function criaCartao(categoria, pergunta, resposta) {
     <p>${resposta}</p>
     </div>
     </div>`
-    
-let respostaEstaVisivel = false
 
-function viraCartao(){
-    respostaEstaVisivel=!respostaEstavisivel
-    cartao.classList.toggle('active',respostaEstaVisivel)
-}
+    let respostaEstaVisivel = false
 
- cartao.appendChild(cartao)   
+    function viraCartao() {
+        respostaEstaVisivel = !respostaEstaVisivel
+        cartao.classList.toggle('active', respostaEstaVisivel)
+    }
 
-container. appendChild(cartao)
+    cartao.addEventListener('click',viraCartao)
+
+    container.appendChild(cartao)
 
 }
 
