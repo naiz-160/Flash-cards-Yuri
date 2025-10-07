b4a149d413768e6340195c39c84a727c738b0dfe
yuri_naiz <naiz.yuri@escola.pr.gov.br>
2025-10-07 09:29:58 -0300

    profe

--- perguntas.js
+++ perguntas.js
@@ -1,59 +1,59 @@
 criaCartao(
-'conhecimento gerais'
-'Quanto tempo a luz do Sol demora para chegar à Terra?'
+'conhecimento gerais',
+'Quanto tempo a luz do Sol demora para chegar à Terra?',
 '8 minutos.'
 )
 
 criaCartao(
-'conhecimento gerais'
-'Em que ano o homem pisou na lua?'
+'conhecimento gerais',
+'Em que ano o homem pisou na lua?',
 '1969.'
 )
 
 criaCartao(
-'conhecimento gerais'
-'Qual ave é capaz de bater as asas até 200 vezes por segundo?'
+'conhecimento gerais',
+'Qual ave é capaz de bater as asas até 200 vezes por segundo?',
 'o beija-flor.'
 )
 
 criaCartao(
-'conhecimento gerais'
-'Quantos ossos temos no nosso corpo?'
+'conhecimento gerais',
+'Quantos ossos temos no nosso corpo?',
 '206.'
 )
 
 criaCartao(
-'conhecimento gerais'
-'Qual é o menor país do mundo?'
+'conhecimento gerais',
+'Qual é o menor país do mundo?',
 'Vaticano.'
 )
 
 criaCartao(
-'conhecimento gerais'
-'Quantas teclas tem um piano?'
+'conhecimento gerais',
+'Quantas teclas tem um piano?',
 '88'
 )
 
 criaCartao(
-'conhecimento gerais'
-'Por qual apelido ficou conhecido o jogador de futebol Edson Arantes?'
+'conhecimento gerais',
+'Por qual apelido ficou conhecido o jogador de futebol Edson Arantes?',
 'Pelé.'
 )
 
 criaCartao(
-'conhecimento gerais'
-'O que é filantropo?'
+'conhecimento gerais',
+'O que é filantropo?',
 ' Pessoa que pratica a caridade.'
 )
 
 criaCartao(
-'conhecimento gerais'
-'Em qual esporte são usadas as mãos para fazer gol?'
+'conhecimento gerais',
+'Em qual esporte são usadas as mãos para fazer gol?',
 'handebol ou polo aquático.'
 )
 
 criaCartao(
-'conhecimento gerais'
-'Qual a montanha mais alta do Brasil?'
+'conhecimento gerais',
+'Qual a montanha mais alta do Brasil?',
 'Pico da Neblina'
 )
